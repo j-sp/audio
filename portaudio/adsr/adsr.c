a23c7d3ccb80e65f7ad287f2d67169b7a942c871
a-sc <adrian.s.c.05@gmail.com>
2020-10-23 18:17:45 +0200

    fixed adsr.c

--- portaudio/adsr/adsr.c
+++ portaudio/adsr/adsr.c
@@ -3,12 +3,15 @@ double adsr(double t, double attack_time, double decay_time, double sustain_time
 
     if (t<=attack_time) {
         return t/attack_time;
-    } elseif(t<=attack_time+decay_time) {
+    }
+    if(t<=attack_time+decay_time) {
         return 1 - ((t-attack_time)*((1-sustain_level)/decay_time));
-    } elseif(t<=attack_time+decay_time+sustain_time){
+    }
+    if(t<=attack_time+decay_time+sustain_time){
         return sustain_level;
-    } elseif(t<=attack_time+decay_time+sustain_time+release_time){
-        return sustain_level - ((t-(attack_time+decay_time+sustain_time))*(sustain_level/release_time))
+    }
+    if(t<=attack_time+decay_time+sustain_time+release_time){
+        return sustain_level - ((t-(attack_time+decay_time+sustain_time))*(sustain_level/release_time));
     } else {
         return 0;
     }
